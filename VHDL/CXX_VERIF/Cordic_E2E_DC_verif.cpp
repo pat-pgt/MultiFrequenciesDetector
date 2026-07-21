81cf0d5e5a7edf9a125f33df7764383d41a0ce24
PatPgt <115630169+pat-pgt@users.noreply.github.com>
2026-07-21 14:58:42 +0200

    CXX verif of Cordic_E2E_DC re-arranged
    * The statistics part has now its own files.
      The goal is to share them with the future Cordic_E2E_lightFilter verification.
    * For each compilation, only the relevant DSn file (from the CXX_RTL Yosys module) is included.
      The compilation time is hugely reduced.

--- VHDL/CXX_VERIF/Cordic_E2E_DC_verif.cpp
+++ VHDL/CXX_VERIF/Cordic_E2E_DC_verif.cpp
@@ -1,163 +1,21 @@
 #include "Cordic_E2E_DC_verif.hxx"
 
+#ifdef Cordic_E2E_DC_CXX_noDS
+#include <Cordic_E2E_DC_CXX_noDS.cpp>
+#elif Cordic_E2E_DC_CXX_DS1
+#include <Cordic_E2E_DC_CXX_DS1.cpp>
+#elif Cordic_E2E_DC_CXX_DS2
+#include <Cordic_E2E_DC_CXX_DS2.cpp>
+#else
+#error "Not all down-sampling rates VHDL CXX tests are compiled"
+#endif
+
+
+#include "Utils_CXX_verif.cpp"
+
+
 #include <getopt.h>
 
-/** @file Cordic_E2E_DC_verif.hxx
- * @brief Run a formal verification
- *
- * There is no more documentation, for future information, see the .hxx file documentation
- */
-
-
-template <typename cxx_reg_type, unsigned short reg_size>
-XY_Data<cxx_reg_type,reg_size>::XY_Data(const int&X_init,const int&Y_init):
-  X_init(X_init),Y_init(Y_init)
-{};
-
-/** @brief InitialValueData constructor
- *
- * Constructs for reg_size equal to 32
- * Other sizes are not yet supported, see in the .hxx file
- */
-template <>
-InitialValueData<int,32>::InitialValueData(const int&X_init,const int&Y_init):
-  XY_Data(X_init,Y_init)
-{
-  // Check if the numbers are 31 bits
-  if ( ( X_init & 0xc0000000 ) == 0x80000000 || ( X_init & 0xc0000000 ) == 0x40000000 )
-	throw length_error("The initial X value is out of range, should be signed 31 bits ");
-  if ( ( Y_init & 0xc0000000 ) == 0x80000000 || ( Y_init & 0xc0000000 ) == 0x40000000 )
-	throw length_error("The initial Y value is out of range, should be signed 31 bits ");
-}
-
-
-template <>
-int InitialValueData<int,32>::Get_X_init_2divided()const
-{
-  return X_init & 0x7fffffff;
-}
-template <>
-int InitialValueData<int,32>::Get_Y_init_2divided()const
-{
-  return Y_init & 0x7fffffff;
-}
-
-
-/** @brief stats class constructor
- *
- */
-template<typename T>stats<T>::stats():
-  nbre_points( 0 ),
-  the_sum_avg( 0 ),the_sum_stddev( 0 ),the_sum_skew( 0 ),the_sum_kurt( 0 ),
-  the_min( numeric_limits<T>::max() ),
-  the_max( numeric_limits<T>::min() ),
-  offset( 0.0 ),
-  normalize( 1.0 )  //,
-  // avg_ratios_between_stats( nullopt )
-{}
-/** @brief stats class constructor
- *
- */
-template<typename T>stats<T>::stats(T* avg_ratios_between_stats):
-  nbre_points( 0 ),
-  the_sum_avg( 0 ),the_sum_stddev( 0 ),the_sum_skew( 0 ),the_sum_kurt( 0 ),
-  the_min( numeric_limits<T>::max() ),
-  the_max( numeric_limits<T>::min() ),
-  offset( 0.0 ),
-  normalize( 1.0 ),
-  avg_ratios_between_stats( avg_ratios_between_stats )
-  {}
-
-/** @brief Add a new value for the statistics
- *
- * It computes all the powers and the additions
- * The code such as X * X has been preferred to pow( x, 2 )
- */
-template<typename T>stats<T>&stats<T>::operator+=(const T&input_val)
-{
-  T val = input_val - offset;
-  val /= normalize;
-  nbre_points += 1;
-  the_sum_avg += val;
-  T val_power_N = val * val;
-  the_sum_stddev += val_power_N;
-  val_power_N *= val;
-  the_sum_skew += val_power_N;
-  val_power_N *= val;
-  the_sum_kurt += val_power_N;  
-  if ( val > the_max )
-	the_max = val;
-  if ( val < the_min )
-	the_min = val;
-  return*this;
-}
-
-
-template<typename T>string stats<T>::Basic_display()
-{
-  T the_avg( the_sum_avg / nbre_points );
-  T the_stddev =  sqrt( the_sum_stddev / nbre_points - the_avg * the_avg ) ;
-
-  // TODO find the formula to get the kurtosis and the skew
-
-  // TODO improve the display
-  if ( avg_ratios_between_stats )
-	{
-	  if ( *(*avg_ratios_between_stats) == 0.0 )
-		*(*avg_ratios_between_stats) = the_avg;
-	  return format("{: E}, {: E}, {:1.3E}, {: E}",
-					the_max - the_min, the_avg, the_stddev, the_avg / *(*avg_ratios_between_stats) );
-	}
-	else
-	{
-	  return format("{: E}, {: E}, {:1.3E},         ",
-					the_max - the_min, the_avg, the_stddev);
-
-	}
-}
-
-template<typename T>string stats<T>::Display_without_offset_normalize()const
-{
-  T the_avg( the_sum_avg / nbre_points );
-  T the_stddev =  sqrt( the_sum_stddev / nbre_points - the_avg * the_avg ) ;
-
-  // TODO find the formula to get the kurtosis and the skew
-
-  // TODO improve the display
-
-  return format("{: E}, {: E}, {:1.3E}",
-				the_max * normalize + offset - the_min * normalize + offset,
-				the_avg * normalize + offset,
-				the_stddev * normalize + offset); 
-}
-
-template<typename T>string stats<T>::Display_arccos_degrees()const
-{
-  T the_avg( the_sum_avg / nbre_points );
-  T the_stddev =  sqrt( the_sum_stddev / nbre_points - the_avg * the_avg ) ;
-
-  // TODO find the formula to get the kurtosis and the skew
-
-  // TODO improve the display
-
-  return format("{: E}, {: E}, {:1.3E}",
-				acos( the_max * normalize + offset) * 360.0 / ( 2.0 * numbers::pi ) -
-				acos( the_min * normalize + offset) * 360.0 / ( 2.0 * numbers::pi ),
-				acos( the_avg * normalize + offset) * 360.0 / ( 2.0 * numbers::pi ),
-				the_stddev * normalize + offset); 
-}
-
-template<typename T>string stats<T>::Display_arccos_Nth_turns()const
-{
-  T the_avg( the_sum_avg / nbre_points );
-
-  // TODO find the formula to get the kurtosis and the skew
-
-  // TODO improve the display
-
-  return format("{: E}",
-				( 2.0 * numbers::pi ) / acos( the_avg * normalize + offset) );
-}
 
 template <typename stats_type, typename stats_long_type,typename cxx_reg_type, unsigned short reg_size>
 void SimulDataType<stats_type,stats_long_type,cxx_reg_type,reg_size>::
---
+++ VHDL/CXX_VERIF/Utils_CXX_verif.hxx
@@ -0,0 +1,192 @@
+#include <algorithm>
+#include <execution>
+#include <chrono>
+#include <iostream>
+#include <limits>
+#include <set>
+#include <map>
+#include <vector>
+#include <format>
+#include <cmath>
+#include <stdexcept>
+#include <optional>
+#include <limits>
+#include <ranges>
+
+using namespace std;
+
+/* @brief Holds an X and a Y value
+ *
+ * The second template member looks like redundant.
+ * Since the interface between the C++ test software and the C++ VHDL emulator
+ * Uses only standard C++ types, the reg_size is not always the size of the reg_type.
+ */ 
+template <typename cxx_reg_type, unsigned short reg_size>
+class XY_Data
+{
+protected:
+  cxx_reg_type X_init;
+  cxx_reg_type Y_init;
+public:
+  XY_Data() = delete;
+  XY_Data(const int&X_init,const int&Y_init);
+
+  XY_Data&operator=(const XY_Data&a){
+	X_init=a.X_init;
+	Y_init=a.Y_init;
+	return*this;
+  }
+
+  constexpr cxx_reg_type value_type()const{};
+  const unsigned short value_size = reg_size;
+
+/** @brief Get the X value based on a standard it format
+   *
+   * @return the value
+   */
+  constexpr int Get_X_full()const{ return X_init; }
+/** @brief Get the Y value based on a standard it format
+   *
+   * @return the value
+   */
+  constexpr int Get_Y_full()const{ return Y_init; }
+
+/** @brief Get the module squared
+ *
+ * The values, in general, are intended to be cast into floating types
+ * Then this function returns the value before the square root
+ */
+  constexpr long long GetModuleSquared()const {  
+	long long X = X_init;
+	long long Y = Y_init;
+	return X * X + Y * Y;
+  }
+  constexpr long double GetCosine(const XY_Data<cxx_reg_type,reg_size>&a)const {
+	long long X1 = X_init;
+	long long Y1 = Y_init;
+	long long X2 = a.X_init;
+	long long Y2 = a.Y_init;
+	long double scalarProduct = X1 * X2 + Y1 * Y2;
+	// Since it has been checked the modules squared are always constant
+	// It is assumed here the sqrt( module_1 ** 2 ).sqrt( module_2 ** 2 ) = module_2 ** 2
+	return scalarProduct / GetModuleSquared();
+  }
+  operator string()const{
+	return format("{},{}\t",X_init,Y_init);
+  }
+  string string_light()const{
+	return format("{: 4},{: 4}\t",X_init/1000000,Y_init/1000000);
+  }
+};
+/* @brief Holds the initial values
+ *
+ * Since the Cordic algorithm grows the values, the input is divided by 2.
+ * The Python emulations validate that, for the 3 first stages done "by hand".\n
+ * Functions are provided for standard IT formats and for the vector size in VHDL
+ * (CXXRTL expects the unused bits are 0).\n
+ * It is a template instantiation. However, for now, it is specific instantiations.
+ */ 
+  template <typename cxx_reg_type, unsigned short reg_size>
+  class InitialValueData : public XY_Data<cxx_reg_type, reg_size>
+{
+public:
+  InitialValueData() = delete;
+  InitialValueData(const int&X_init,const int&Y_init);
+
+  long double module_value_type()const;
+  /** @brief Get the X value for the VHDL
+   *
+   * @return the value
+   */
+  constexpr cxx_reg_type Get_X_init_2divided()const;
+/** @brief Get the Y value for the VHDL
+   *
+   * @return the value
+   */
+  constexpr cxx_reg_type Get_Y_init_2divided()const;
+  /** @brief Get size to compare with the VHDL
+   *
+   * This function returns the number of BITS.
+   */
+  const unsigned char GetRegSize()const{ return reg_size;}
+};
+
+/** @brief Computes the statistics of the results
+ *
+ * This class is generic as it has no direct connection with the VHDL.\n
+ * The type should be enough large to avoid any precision lost.\n
+ * If the average value is an expected value, it is a good idea to set here.
+ * All the computation are going to be done on the difference to improve the precision.
+ * If needed, the offset can be add back on the result.
+ */
+template<typename T>
+class stats
+{
+  unsigned int nbre_points;
+  T the_sum_avg, the_sum_stddev, the_sum_skew, the_sum_kurt;
+  T the_min, the_max;
+  T normalize, offset;
+  optional< T* >avg_ratios_between_stats;
+public:
+  typedef T value_type;
+
+  stats();
+  stats(T* avg_ratios_between_stats);
+
+  stats<T>&operator+=(const T&);
+  constexpr void SetNormalize(const T&normalize){
+	if ( nbre_points > 0 )
+	  throw invalid_argument("Internal: The normalization can NOT be changed after the first value");
+	this -> normalize = normalize;
+  }
+  constexpr void SetOffset(const T&offset){
+	if ( nbre_points > 0 )
+	  throw invalid_argument("Internal: The offset can NOT be changed after the first value");
+	this -> offset = offset;
+  }
+  string Basic_display();
+  string Display_without_offset_normalize()const;
+  string Display_arccos_degrees()const;
+  operator unsigned int()const{return nbre_points;}
+  string Display_arccos_Nth_turns()const;
+};
+
+/** @brief Stores the data to count the number of times a vector passes the origin.
+ *
+ * The assume is the spin, at each iteration, is always less than PI.
+ * The last high bit is stored. If it was 1 and now it is 0,
+ *   the counter is incremented
+ */
+template<typename cxx_reg_type,unsigned short reg_size>
+class Axis_counter
+{
+  const cxx_reg_type high_bit_mask;
+  bool last_sign;
+  unsigned long the_counter;
+public:
+  // TODO TODO TODO set the bit properly  in order to run with other sizes than 32
+  Axis_counter():high_bit_mask(0x80000000),last_sign(false),the_counter(0){}
+  constexpr Axis_counter<cxx_reg_type,reg_size>&operator+=(const cxx_reg_type&the_input){
+	bool this_sign;
+	if ( ( the_input & high_bit_mask ) == 0 )
+	  this_sign = false;
+	else
+	  this_sign = true;
+	if ( this_sign == false && last_sign == true )
+	  the_counter += 1;
+	last_sign = this_sign;
+	return*this;
+ }
+  constexpr operator const unsigned long()const{return the_counter;}
+};
+/** @brief Holds the last angle, the statistics and the number of X axis passes
+ */
+template <typename stats_type, typename cxx_reg_type, unsigned short reg_size>
+struct Z_spin_data
+{
+  cxx_reg_type last_Z;
+  stats<stats_type> the_stats;
+  Axis_counter<cxx_reg_type,reg_size> the_X_counter;
+  Z_spin_data()=delete;
+  Z_spin_data(const cxx_reg_type&last_Z,const stats<stats_type>&the_stats): last_Z(last_Z),the_stats(the_stats){}
+};
